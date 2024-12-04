6184009f992421a7ba732943bf9810f96b9e3ecf
Ramiro Estigarribia <ramiroec@gmail.com>
2024-12-04 18:08:04 -0300

    "Update app run configuration to use environment variable for port and bind to all available network interfaces"

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def delete_videogame(id):
     return jsonify({'message': 'Videogame not found'}), 404
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    port = int(os.environ.get('PORT', 3000))  
+    app.run(host='0.0.0.0', port=port)
